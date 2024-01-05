4742b78f717f42cc3a937bf1540c78690ec55496
BESSTSU <152589209+BESSTSU@users.noreply.github.com>
2024-01-05 21:55:32 +0700

    lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+T sum;
+int x;    
+for(int i=0;i<N-1;i++)
+        {    
+           for(int j = 0;j <=i;j++)
+            {
+                if(d[j]<d[i+1])
+                {   sum=d[i+1];
+                    x=i;
+                    for(int k =j;k<=i;k++)
+                    {
+                    d[x+1]=d[x];
+                    x--;
+                    }
+                    d[j]=sum;    
+                }
+            } 
+            cout<<"Pass "<<i+1<<":";
+            for(int M=0;M<N;M++)
+            {cout << d[M]<<" ";}  
+            cout<<"\n";  
+        }
 }
 
 int main(){
